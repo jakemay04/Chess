352b9b0ed46cfbd8c7d1b51bad69ca4c3b45ff37
jakemay04 <jmay90@byu.edu>
2026-02-26 23:37:20 -0700

    create insert auth method

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,5 +1,29 @@
 package dataaccess;
 
-public class MemoryAuthDAO implements AuthDAO{
+import model.AuthData;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemoryAuthDAO implements AuthDAO{
+    private Map<String, String> auth = new HashMap<String, String>();
+
+    public void insertAuth(AuthData a) throws DataAccessException {
+        if (a.username() != null) {
+            auth.put(a.username(), a.authToken());
+        }
+        else {
+            throw new DataAccessException("Invalid session");
+        }
+    }
+
+    public void getAuth(AuthData a) throws DataAccessException {
+        AuthDAO.super.getAuth(a);
+    }
+
+    public void clear() {
+        auth = null;
+    }
 }
