5b0d94da471d27be3b6bf293725e2aae7c645d4e
jakemay04 <jmay90@byu.edu>
2026-03-01 23:23:25 -0700

    setup user test class

---
+++ server/src/test/java/service/UserServiceTests.java
@@ -0,0 +1,12 @@
+package service;
+
+import dataaccess.AuthDAO;
+import dataaccess.UserDAO;
+
+public class UserServiceTests {
+    static UserDAO userDAO;
+    static AuthDAO authDAO;
+    static UserService userService;
+
+
+}
