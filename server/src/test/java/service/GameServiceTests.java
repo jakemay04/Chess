6536fe2cf22943b8bf1c9108467bed777196b23a
jakemay04 <jmay90@byu.edu>
2026-03-02 19:40:13 -0700

    create game service object for testing

--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -9,17 +9,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GameServiceTests {
     static UserDAO userDAO;
     static AuthDAO authDAO;
+    static GameDAO gameDAO;
     static UserService userService;
+    static GameService gameService;
 
     @BeforeEach
     void Setup() {
         userDAO = new MemoryUserDAO();
         authDAO = new MemoryAuthDAO();
-        userService = new UserService(userDAO, authDAO);
+        gameDAO = new MemoryGameDAO();
+        gameService = new GameService(userDAO,authDAO,gameDAO);
     }
     @Test
     void createGameSuccess() throws DataAccessException {
         var result = userService.register(new RegisterRequest("test","pass","test@test.com"));
+        var createGame = gameService.createGame(CreateGameRequest())
         assertEquals("test", result.username());
     }
 }
