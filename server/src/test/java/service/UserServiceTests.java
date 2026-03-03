b475e2784eb3f4874869af98087235ebac957f4c
jakemay04 <jmay90@byu.edu>
2026-03-02 19:23:27 -0700

    register and login test methods

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -1,12 +1,49 @@
 package service;
 
-import dataaccess.AuthDAO;
-import dataaccess.UserDAO;
+import dataaccess.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserServiceTests {
     static UserDAO userDAO;
     static AuthDAO authDAO;
     static UserService userService;
 
+    @BeforeEach
+    void Setup() {
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        userService = new UserService(userDAO, authDAO);
+    }
 
+    @Test
+    void registerSuccess() throws DataAccessException {
+        var result = userService.register(new RegisterRequest("test","pass","test@test.com"));
+        assertEquals("test", result.username());
+    }
+
+    @Test
+    void registerFailed() throws DataAccessException {
+        var result = userService.register(new RegisterRequest("test","pass","test@test.com"));
+        var duplicate = new RegisterRequest("test", "wrongpass", "wrong@test.com");
+        try {
+            userService.register(duplicate);
+            fail("Expected exception to throw");
+        } catch (DataAccessException e) {
+            assertEquals("Error: already taken", e.getMessage());
+        }
+    }
+
+    @Test
+    void loginSuccess() throws DataAccessException {
+        var result = userService.register(new RegisterRequest("test","pass","test@test.com"));
+        var loginResult = userService.login(new LoginRequest("test", "pass"));
+        assertNotNull(loginResult.authToken());
+    }
+
+    void loginWrongPass() throws DataAccessException {
+        var result = userService.register(new RegisterRequest("test","pass","test@test.com"));
+    }
 }
