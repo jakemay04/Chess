5f6c6f08273e67393cb50742bbb4ef09a8929173
jakemay04 <jmay90@byu.edu>
2026-03-02 19:36:20 -0700

    initialize gameservice test file

--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -1,4 +1,25 @@
 package service;
 
+import dataaccess.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GameServiceTests {
+    static UserDAO userDAO;
+    static AuthDAO authDAO;
+    static UserService userService;
+
+    @BeforeEach
+    void Setup() {
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        userService = new UserService(userDAO, authDAO);
+    }
+    @Test
+    void createGameSuccess() throws DataAccessException {
+        var result = userService.register(new RegisterRequest("test","pass","test@test.com"));
+        assertEquals("test", result.username());
+    }
 }
