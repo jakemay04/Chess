b0505c965889998f56a3b4e75a26f8c16bdb1cb0
jakemay04 <jmay90@byu.edu>
2026-03-24 17:46:15 -0600

    setup facade test cases

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -7,12 +7,14 @@ import server.Server;
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + port")
     }
 
     @AfterAll
@@ -25,5 +27,42 @@ public class ServerFacadeTests {
     public void sampleTest() {
         Assertions.assertTrue(true);
     }
+
+    @Test
+    public void registerTestSuccess() {}
+
+    @Test
+    public void registerTestFail() {}
+
+    @Test
+    public void loginTestSuccess() {}
+
+    @Test
+    public void loginTestFail() {}
+
+    @Test
+    public void joinTestSuccess() {}
+
+    @Test
+    public void joinTestFail() {}
+
+    @Test
+    public void createTestSuccess() {}
+
+    @Test
+    public void createTestFail() {}
+
+    @Test
+    public void listTestSuccess() {}
+
+    @Test
+    public void listTestFail() {}
+
+    @Test
+    public void logoutTestSuccess() {}
+
+    @Test
+    public void logoutTestFail() {}
+
 
 }
