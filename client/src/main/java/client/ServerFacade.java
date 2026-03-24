c8d95e21e249887beed8f788687d54c19be3c36c
jakemay04 <jmay90@byu.edu>
2026-03-24 00:03:54 -0600

    finish endpoint facade methods

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -30,8 +30,25 @@ public class ServerFacade {
         var response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
         return handleResponse(response, RegisterResult.class);
     }
-    public CreateGameResult createGame(CreateGameRequest request) {}
-    public ListGamesResult listGames(ListGamesRequest request) {}
-    public void logout(LogoutRequest request) {}
+
+    public CreateGameResult createGame(CreateGameRequest request) {
+        var httpRequest = buildRequest("POST", "/game", request);
+        var response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        return handleResponse(response, RegisterResult.class);
+    }
+
+    public ListGamesResult listGames(ListGamesRequest request) {
+        var httpRequest = buildRequest("GET", "/game", request);
+        var response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        return handleResponse(response, RegisterResult.class);
+    }
+
+    public void logout(LogoutRequest request) {
+        var httpRequest = buildRequest("DELETE", "/session", request);
+        var response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        return handleResponse(response, RegisterResult.class);
+    }
+
+    //helper functions
 
 }
