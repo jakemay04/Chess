54e6e6da1133c19f4d94492316ff5c01e87c07ba
jakemay04 <jmay90@byu.edu>
2026-03-24 14:31:54 -0600

    Setup draw board, create functionality for printing based on teamcolor

--- client/src/main/java/ui/DrawBoard.java
+++ client/src/main/java/ui/DrawBoard.java
@@ -1,8 +1,36 @@
 package ui;
 
+import chess.ChessBoard;
 import chess.ChessGame;
 
 public class DrawBoard {
-    public static void printOutBoard(ChessGame game, String playerColor) {
+    ChessGame game;
+    String playerColor;
+
+    public DrawBoard(ChessGame game, String playerColor) {
+        this.game = game;
+        this.playerColor = playerColor;
     }
-}
+
+    public static void printOutBoard(ChessGame game, String playerColor) {
+        ChessBoard board = game.getBoard();
+        String[] columns = {"a","b","c","d","e","f","g","h"};
+
+        if (playerColor.equals("WHITE")) {
+            for (int row = 7; row >= 0; row--) {
+
+            }
+        } else if (playerColor.equals("BLACK")) {
+            for (int row = 0; row <= 7; row++) {
+
+            }
+        }
+    }
+
+    private static void printRow (ChessBoard board, int row, String playerColor) {
+
+    }
+
+    private static void printSquare (ChessBoard board, int row, int col) {
+
+    }
