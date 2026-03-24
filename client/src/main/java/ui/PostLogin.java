85aaeb942d8c80869ebb3cfba99be36e8f733c23
jakemay04 <jmay90@byu.edu>
2026-03-23 21:11:42 -0600

    Setting up Post Login class like prelogin

--- client/src/main/java/ui/PostLogin.java
+++ client/src/main/java/ui/PostLogin.java
@@ -1,4 +1,41 @@
 package ui;
 
 public class PostLogin {
+
+    private final ServerFacade facade;
+
+    public PostLogin(ServerFacade facade) {
+        this.facade = facade;
+    }
+
+    static String eval(String line) {
+        if (line.equals("Help") || line.equals("help")) {
+            System.out.println("Here is how to navigate the Chess Game: ");
+            System.out.println("To Logout, simply type Logout");
+            System.out.println("To start a new game, type Create");
+            System.out.println("If you would like to see all games in session, type List");
+            System.out.println("If you would like to Join a Game from the list of games,");
+            System.out.println("Type Join. You will then be prompted with the game number and the color you wish to be.");
+            System.out.println("If you would like to Observe a game from the list of games, type Observe");
+            System.out.println("You will then be prompted for the game number");
+            System.out.println("If you would like to quit the program, simply type Quit.");
+
+
+        } else if (line.equalsIgnoreCase("Register")) {
+            //call logout from server facade
+            return line;
+        } else if (line.equalsIgnoreCase("Create")) {
+            //call Create Game from server facade
+        } else if (line.equalsIgnoreCase("List")) {
+            //call List Game from server facade
+        } else if (line.equalsIgnoreCase("Join")) {
+            //call Join Game from server facade
+        } else if (line.equalsIgnoreCase("Quit")) {
+            return "Quit";
+        } else {
+            System.out.println("Unknown command. Type 'Help' for options.");
+        }
+        return line;
+    }
+
 }
