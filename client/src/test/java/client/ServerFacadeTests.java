1d6d555b7aabee893cfb08b5d17dd58dc8d03974
jakemay04 <jmay90@byu.edu>
2026-03-24 18:11:21 -0600

    add register tests and login tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,15 +1,21 @@
 package client;
 
+import exception.ResponseException;
 import org.junit.jupiter.api.*;
-import records.RegisterRequest;
+import records.*;
 import server.Server;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
     private static ServerFacade facade;
-    private final RegisterRequest testUser = new RegisterRequest("user1", "password", "user1@email.com");
+    private final RegisterRequest testRegister =
+            new RegisterRequest("user1", "password", "user1@email.com");
+    private final LoginRequest testLogin =
+            new LoginRequest("user1", "password");
+
 
     @BeforeAll
     public static void init() {
@@ -30,15 +36,30 @@ public class ServerFacadeTests {
     }
 
     @Test
-    public void registerTestSuccess() {
-        var result = facade.register(new testuser)
+    public void registerTestSuccess() throws exception.ResponseException {
+        var result = facade.register(testRegister);
+        assertNotNull(result);
+        assertNotNull(result.authToken());
     }
 
     @Test
-    public void registerTestFail() {}
+    public void registerTestFail() throws exception.ResponseException {
+        var result = facade.register(testUser);
+        try {
+            var resultFail = facade.register(testRegister);
+            fail("Test should have failed here");
+        } catch (ResponseException e) {
+            assertNotNull(e.getMessage());
+        }
+    }
 
     @Test
-    public void loginTestSuccess() {}
+    public void loginTestSuccess() throws ResponseException {
+        var result = facade.register(testRegister);
+        var login = facade.login()
+        assertNotNull(result);
+        assertNotNull(result.authToken());
+    }
 
     @Test
     public void loginTestFail() {}
