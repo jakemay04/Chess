4262dc60c1bf830557f3597da218068d58d414a4
jakemay04 <jmay90@byu.edu>
2026-04-07 20:51:54 -0600

    move MakeMove class to seperate file in shared. add gson deserialization and method calls

--- server/src/main/java/server/WebsocketHandler.java
+++ server/src/main/java/server/WebsocketHandler.java
@@ -2,6 +2,8 @@ package server;
 
 import chess.ChessMove;
 import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import dataaccess.UserDAO;
 import exception.ResponseException;
 import io.javalin.websocket.WsCloseContext;
 import io.javalin.websocket.WsCloseHandler;
@@ -9,16 +11,26 @@ import io.javalin.websocket.WsConnectContext;
 import io.javalin.websocket.WsConnectHandler;
 import io.javalin.websocket.WsMessageContext;
 import io.javalin.websocket.WsMessageHandler;
+import model.UserData;
 import org.eclipse.jetty.websocket.api.Session;
 import server.ConnectionManager;
+import service.UserService;
+import websocket.commands.MakeMoveCommand;
 import websocket.messages.ServerMessage;
 import websocket.commands.UserGameCommand;
+import com.google.gson.Gson;
 
 import java.io.IOException;
 
 public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
 
     private final ConnectionManager connections = new ConnectionManager();
+    private final Gson gson = new Gson();
+    private final UserDAO userDAO;
+
+    public WebSocketHandler(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
 
     @Override
     public void handleConnect(WsConnectContext ctx) {
@@ -31,10 +43,13 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
         try {
             UserGameCommand action = new Gson().fromJson(ctx.message(), UserGameCommand.class);
             switch (action.getCommandType()) {
-                case CONNECT -> connect(action.authToken(), action.gameID(), ctx.session);
-                case LEAVE -> leave(action.playerName(), ctx.session);
-                case RESIGN -> resign(action.playerName(), ctx.session);
-                case MAKE_MOVE -> makeMove(action.playerName(), move, ctx.session);
+                case CONNECT -> connect(action.getAuthToken(), action.getGameID(), ctx.session);
+                case LEAVE -> leave(action.getAuthToken(), action.getGameID(), ctx.session);
+                case RESIGN -> resign(action.getAuthToken(), action.getGameID(), ctx.session);
+                case MAKE_MOVE -> {
+                    MakeMoveCommand moveAction = gson.fromJson(ctx.message(), MakeMoveCommand.class);
+                    makeMove(moveAction.getAuthToken(), moveAction.getGameID(), moveAction.getMove(), ctx.session);
+                }
 
             }
         } catch (IOException ex) {
@@ -47,21 +62,24 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
         System.out.println("Websocket closed");
     }
 
-    private void connect(String playerName, Session session) throws IOException {
+    private void connect(String authToken, Integer gameID, Session session) throws IOException, DataAccessException {
         connections.add(session);
+        UserData playerName = userDAO.getUser(authToken);
         var message = String.format("%s has entered the game", playerName);
         var serverMessage = new ServerMessage(ServerMessage.Type.CONNECT, message);
         connections.broadcast(session, serverMessage);
     }
 
-    private void leave(String playerName, Session session) throws IOException {
+    private void leave(String authToken, Integer gameID, Session session) throws IOException, DataAccessException {
+        UserData playerName = userDAO.getUser(authToken);
         var message = String.format("%s has left the game", playerName);
         var serverMessage = new ServerMessage(ServerMessage.Type.LEAVE, message);
         connections.broadcast(session, serverMessage);
         connections.remove(session);
     }
 
-    private void resign(String playerName, Session session) throws IOException {
+    private void resign(String authToken, Integer gameID, Session session) throws IOException, DataAccessException {
+        UserData playerName = userDAO.getUser(authToken);
         var message = String.format("%s has resigned", playerName);
         var serverMessage = new ServerMessage(ServerMessage.Type.RESIGN, message);
         connections.broadcast(session, serverMessage);
@@ -70,27 +88,12 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
 
     }
 
-    private void makeMove(String playerName, ChessMove move, Session session) throws IOException {
+    private void makeMove(String authToken, Integer gameID, ChessMove move, Session session) throws IOException, DataAccessException {
+        UserData playerName = userDAO.getUser(authToken);
         var message = String.format("%s has resigned", playerName);
         var serverMessage = new ServerMessage(ServerMessage.Type.MAKE_MOVE, message);
         //call make move from server
         connections.broadcast(session, serverMessage);
     }
-
-}
-
-public class MakeMoveCommand extends UserGameCommand {
-
-    private final ChessMove move;
-
-    public MakeMoveCommand(String authToken, Integer gameID, ChessMove move) {
-        super(CommandType.MAKE_MOVE, authToken, gameID);
-        this.move = move;
-    }
-
-    public ChessMove getMove() {
-        return move;
-    }
-
 
 }
