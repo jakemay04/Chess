40bb65d0c221a9615c8aca5b1cdb568a3390d025
jakemay04 <jmay90@byu.edu>
2026-04-09 01:58:15 -0600

    add coonstructers and initiate string eval method

--- client/src/main/java/ui/GameUI.java
+++ client/src/main/java/ui/GameUI.java
@@ -1,8 +1,44 @@
 package ui;
 
+import client.ServerFacade;
+import client.WebSocketFacade;
+import model.GameData;
+
+import java.util.Scanner;
+
 public class GameUI {
 
+    private final ServerFacade facade;
+    private final String authToken;
+    private final int gameID;
+    private final String playerColor;
+    private final Scanner scanner = new Scanner(System.in);
+    private WebSocketFacade wsFacade;
+    private GameData gameData;
 
+    public GameUI(ServerFacade facade, String authToken, int gameID, String playerColor,
+                  String url)
+            throws exception.ResponseException {
+
+        this.facade = facade;
+        this.authToken = authToken;
+        this.gameID = gameID;
+        this.playerColor = playerColor;
+        this.wsFacade = new WebSocketFacade(url, this);
+        //immedietly join game when entering
+        wsFacade.joinGame(authToken, gameID);
+    }
+
+    public String evalInput(String line) {
+        if (line.equalsIgnoreCase("help")) {
+            help();
+        }
+        else if (line.equalsIgnoreCase("redraw")) {
+            if (gameData != null) {
+                DrawBoard.printOutBoard(gameData.game(),playerColor);
+            }
+        }
+    }
 
     private void help() {
         System.out.println("Possible options: ");
