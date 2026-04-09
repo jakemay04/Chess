670b8ad5fed9d2a04de39e012074fa83ec905db6
jakemay04 <jmay90@byu.edu>
2026-04-09 05:40:56 -0600

    code quality

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Repl {
 
-    private static final String urlLocal = "http://localhost:8080";
+    private static final String URL = "http://localhost:8080";
     private final ServerFacade facade = new ServerFacade("http://localhost:8080");
     private final PreLogin preLogin = new PreLogin(facade);
     private PostLogin postLogin = null;
@@ -38,7 +38,7 @@ public class Repl {
                     if (result.startsWith("loggedin:")) {
                         authToken = result.split(":")[1];
                         loggedIn = true;
-                        postLogin = new PostLogin(facade, authToken, urlLocal);
+                        postLogin = new PostLogin(facade, authToken, URL);
                         result = "You are now Logged in! Type Help, List, Join, Create, Observe or Quit to continue";
                     }
                 }
