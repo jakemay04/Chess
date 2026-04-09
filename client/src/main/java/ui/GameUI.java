7270ed77be5ac1bc9850234caea0941a8c5b5f1d
jakemay04 <jmay90@byu.edu>
2026-04-09 02:25:21 -0600

    add notify handler method, change string to char for chess position and add loop exit for leave

--- client/src/main/java/ui/GameUI.java
+++ client/src/main/java/ui/GameUI.java
@@ -1,16 +1,19 @@
 package ui;
 
+import chess.ChessMove;
 import chess.ChessPosition;
+import client.MsgHandler;
 import client.ServerFacade;
 import client.WebSocketFacade;
 import exception.ResponseException;
 import model.GameData;
+import websocket.messages.ServerMessage;
 
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
 
-public class GameUI {
+public class GameUI implements MsgHandler {
 
     private final ServerFacade facade;
     private final String authToken;
@@ -44,18 +47,26 @@ public class GameUI {
         } else if (line.equalsIgnoreCase("leave")) {
             try {
                 wsFacade.leaveGame(authToken, gameID);
+                return "left";
             } catch (Exception e) {
                 return e.getMessage();
             }
 
         } else if (line.equalsIgnoreCase("move")) {
-            System.out.println("From (Ex. a2: ");
-            String from = scanner.nextLine();
+            try {
+                System.out.println("From (Ex. a2): ");
+                String from = scanner.nextLine();
 
-            System.out.println("To (Ex. a4: ");
-            String to = scanner.nextLine();
+                System.out.println("To (Ex. a4): ");
+                String to = scanner.nextLine();
+                ChessPosition start = chessPosition(from);
+                ChessPosition end = chessPosition(to);
+                ChessMove move = new ChessMove(start, end, null);
 
-
+                wsFacade.makeMove(authToken, gameID, move);
+            } catch (Exception e) {
+                return e.getMessage();
+            }
 
         } else if (line.equalsIgnoreCase("resign")) {
             try {
@@ -88,11 +99,23 @@ public class GameUI {
 
     private ChessPosition chessPosition(String pos) {
         Map<Character, Integer> columns = Map.of(
-                "a", 1, "b", 2, "c", 3, "d", 4,
-                "e",5,"f",6,"g",7,"h", 8
+                'a', 1, 'b', 2, 'c', 3, 'd', 4,
+                'e',5,'f',6,'g',7,'h', 8
         );
         int row = Integer.parseInt(String.valueOf(pos.charAt(1)));
         int col = columns.get(pos.charAt(0));
         return new ChessPosition(row, col);
+    }
+
+    @Override
+    public void notify(ServerMessage message) {
+        switch (message.getServerMessageType()) {
+            case NOTIFICATION -> System.out.println("\n" + message.getMessage());
+            case ERROR -> System.out.println("\nError: " + message.getErrorMessage());
+            case LOAD_GAME -> {
+                gameData = message.getGame();
+                DrawBoard.printOutBoard(gameData.game(), playerColor);
+            }
+        }
     }
 }
