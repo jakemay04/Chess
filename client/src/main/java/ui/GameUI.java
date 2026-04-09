ed20bd672fd64dd3940d8e622e505512e64eb5f9
jakemay04 <jmay90@byu.edu>
2026-04-09 02:19:08 -0600

    string parsing for make move inputs from commandline

--- client/src/main/java/ui/GameUI.java
+++ client/src/main/java/ui/GameUI.java
@@ -1,10 +1,13 @@
 package ui;
 
+import chess.ChessPosition;
 import client.ServerFacade;
 import client.WebSocketFacade;
 import exception.ResponseException;
 import model.GameData;
 
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 
 public class GameUI {
@@ -46,6 +49,13 @@ public class GameUI {
             }
 
         } else if (line.equalsIgnoreCase("move")) {
+            System.out.println("From (Ex. a2: ");
+            String from = scanner.nextLine();
+
+            System.out.println("To (Ex. a4: ");
+            String to = scanner.nextLine();
+
+
 
         } else if (line.equalsIgnoreCase("resign")) {
             try {
@@ -74,5 +84,15 @@ public class GameUI {
         System.out.println("Make Move - declare a move against your opponent");
         System.out.println("Resign - forfeit the current game let your opponent win");
         System.out.println("Highlight Legal Moves - redraw the board with the highlighted possible moves");
+    }
+
+    private ChessPosition chessPosition(String pos) {
+        Map<Character, Integer> columns = Map.of(
+                "a", 1, "b", 2, "c", 3, "d", 4,
+                "e",5,"f",6,"g",7,"h", 8
+        );
+        int row = Integer.parseInt(String.valueOf(pos.charAt(1)));
+        int col = columns.get(pos.charAt(0));
+        return new ChessPosition(row, col);
     }
 }
